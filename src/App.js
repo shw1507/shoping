875c42648f26e8f4e5f34386266d775d9a672f5d
신현우 <qnfskqkddlek@naver.com>
2024-05-28 22:30:56 +0900

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Container, Nav} from 'react-bootstrap'
+import { useState } from "react";
+import data from "./data";
 
 function App() {
+
+  let [shoes,setShoes] = useState(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <Navbar bg="lignt" variant="lignt">
+        <Container>
+          <Navbar.Brand href="#home">shoeShop</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+            </Nav>
+        </Container>
+      </Navbar>
+      <div className="main-bg"></div>
+      <div className="container" />
+        <div className="row">
+          <div className="col-md-4">
+            <img src="/shoes1.jpg" alt="shoes" style={{width: "80%"}}/>
+            <h4>{shoes[0].title}</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src="/shoes2.jpg" alt="shoes" style={{width: "80%"}}/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src="/shoes3.jpg" alt="shoes" style={{width: "80%"}}/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+        </div>
+      </div>
   );
 }
 
